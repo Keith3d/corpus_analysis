8976fb287005ed3ca2599e74222d1e857dc13228
Keith Wilson <keith3d@mac.com>
2021-09-10 15:45:10 +0100

    finished session 1

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-print(1)
-print(2)
-print(3)
+length_limit = 500
+# Read in content from file
+f = open("data/full-qn/QN35_1.txt", "r")
+# Assign text content from file as a string variable
+data = f.read()
+# Split string into list of words (removing unwanted characters)
+data_words = data.replace("\n", " ").replace(".", "").split(" ")
+# Clean the list, removing empty items
+data_words_clean = list(filter(("").__ne__, data_words))
+# Data lengths
+data_words_length = len(data_words_clean)
+data_length = len(data)
+# 
+print(data_length)
